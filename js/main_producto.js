ae96d2777477c3ea9e6f23bec1a1ecd878edf4ca
Gonzalo Cortes <gonzalocorte99@gmail.com>
2023-01-13 21:04:44 -0300

    arreglando errores

--- js/main_producto.js
+++ js/main_producto.js
@@ -1,4 +1,4 @@
-const rederProducto = () => {
+const renderProducto = () => {
     const libros = cargarProductos();
     let salida = `<div class="col-md-6 text-center">
         <img src="imagenes/${libros.imagen}" alt="${libros.nombre}" class="img-fluid" title="${libros.nombre}" />
@@ -8,11 +8,11 @@ const rederProducto = () => {
       <h3>${libros.genero}</h3>
       <p>${libros.descripcion}</p>
       <p>$${libros.precio}</p>
-      <p><a href="#" class="btn btn-warning" onClick="agregarAlCarrito(${libros.id}); title="Agregar al Carrito">Agregrar (+)</a></p>
+      <p><a href="#" class="btn btn-warning" onClick="agregarAlCarrito(${libros.id});" title="Agregar al Carrito">Agregrar (+)</a></p>
     </div>`;
 
     document.getElementById("libro").innerHTML = salida;
 }
 
-rederProducto();
+renderProducto();
 renderBotonCarrito();
